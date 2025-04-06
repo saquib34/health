732749101085d20990346c862d5cf6297d19e2a0
MOHAMMAD SAQUIB DAIYAN <85185972+saquib34@users.noreply.github.com>
2025-04-06 15:14:31 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // API base URL - configured in .env file
-const API_URL = 'https://bf15-15-206-74-169.ngrok-free.app/api/';
+const API_URL = 'https://1074-34-55-58-6.ngrok-free.app/api/';
 
 // Create axios instance
 const api = axios.create({
